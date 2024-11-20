ef8dd11ae9fa2700c75dbb258211329bc7daefc6
Runethesilly <rune77263@gmail.com>
2024-11-20 08:52:28 -0800

    Screen wrap complete

--- Assets/Scripts/ScreenWrap.cs
+++ Assets/Scripts/ScreenWrap.cs
@@ -6,8 +6,8 @@ public class ScreenWrap : MonoBehaviour
 {
     public float xMin;
     public float xMax;
-    public float yMin;
-    public float yMax;
+    public float zMin;
+    public float zMax;
 
     // Start is called before the first frame update
     void Start()
@@ -21,20 +21,38 @@ public class ScreenWrap : MonoBehaviour
         if (transform.position.x > xMax)
         {
             transform.position = new Vector3(
-                transform.position.x,
+                xMin,
                 transform.position.y,
-                xMin
-                );
-        }
-        if (transform.position.x > xMin)
-        {
-            transform.position = new Vector3(
-            xMax,
-                transform.position.x,
-                transform.position.y
+                transform.position.z
                 );
         }
 
+        if (transform.position.x < xMin)
+        {
+            transform.position = new Vector3(
+            xMax,
+                transform.position.y,
+                transform.position.z
+                );
+        }
+
+        if (transform.position.z > zMax)
+        {
+            transform.position = new Vector3(
+                transform.position.x,
+                transform.position.y,
+                zMin
+                );
+        }
+
+        if (transform.position.z < zMin)
+        {
+            transform.position = new Vector3(
+                transform.position.x,
+                transform.position.y,
+                zMax
+                );
+        }
 
 
     }
