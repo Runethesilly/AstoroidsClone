cca46150370ffa1ef02a76b96dc88db8fa88f44d
Runethesilly <rune77263@gmail.com>
2024-11-26 09:24:42 -0800

    progress on wave spawner

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    public float Rock;
+    public GameObject[] asteroidPrefab;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
 
     // Start is called before the first frame update
     void Start()
@@ -15,9 +17,12 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
+        if(Input.GetKeyDown(KeyCode.S))
         {
-            
+            //Randomly generate spawn location
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            int asteroidIndex = Random.Range(0, asteroidPrefab.Length);
+            Instantiate(asteroidPrefab[asteroidIndex], spawnPos, asteroidPrefab[asteroidIndex].transform.rotation);
         }
     }
 }
