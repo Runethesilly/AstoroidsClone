cba8e1ba4d414aef13011c2a31fdcc57fd4c9854
Runethesilly <rune77263@gmail.com>
2024-11-26 10:27:46 -0800

    wave spawner needs some tweeking but works

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -7,6 +7,7 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] asteroidPrefab;
     private float spawnRangeX = 20;
     private float spawnPosZ = 20;
+    public int AsteroidCount;
 
     // Start is called before the first frame update
     void Start()
@@ -17,12 +18,14 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.S))
+       AsteroidCount = GameObject.FindObjectsByType<AsteroidController>(0).Length;
+
+        if(AsteroidCount == 0)
         {
-            //Randomly generate spawn location
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+           Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
             int asteroidIndex = Random.Range(0, asteroidPrefab.Length);
             Instantiate(asteroidPrefab[asteroidIndex], spawnPos, asteroidPrefab[asteroidIndex].transform.rotation);
         }
+
     }
 }
